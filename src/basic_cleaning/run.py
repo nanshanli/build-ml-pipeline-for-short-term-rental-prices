8299108e2916986277d13cb6565d3a7a27fddbf5
nanshanli <nanshan.li@toyota.com>
2023-03-18 16:32:33 -0400

    Step 2: Data Cleaning

---
+++ src/basic_cleaning/run.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python
+"""
+Download from W&B the raw dataset and apply some basic data cleaning, exporting the result to a new artifact
+"""
+import argparse
+import logging
+import wandb
+import pandas as pd
+
+
+logging.basicConfig(level=logging.INFO, format="%(asctime)-15s %(message)s")
+logger = logging.getLogger()
+
+
+def go(args):
+
+    run = wandb.init(job_type="basic_cleaning")
+    run.config.update(args)
+
+    # Download input artifact. This will also log that this script is using this
+    # particular version of the artifact
+    artifact_local_path = run.use_artifact(args.input_artifact).file()
+
+    # Load dataset
+    data = pd.read_csv(artifact_local_path)
+    df = data.copy()
+
+    # Clean dataset by removing outliers
+    min_price = float(args.min_price)
+    max_price = float(args.max_price)
+    idx = df['price'].between(min_price, max_price)
+    df = df[idx].copy()
+
+    # Saving to CSV
+    df.to_csv(args.output_artifact, index=False)
+    logger.info('Dataset saved.')
+
+    # Upload to W&B
+    artifact = wandb.Artifact(
+        args.output_artifact,
+        type=args.output_type,
+        description=args.output_description,
+    )
+    artifact.add_file("clean_sample.csv")
+    run.log_artifact(artifact)
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description="A very basic data cleaning")
+
+    parser.add_argument(
+        "--input_artifact", 
+        type=str,
+        help="Input artifact",
+        required=True
+    )
+
+    parser.add_argument(
+        "--output_artifact", 
+        type=str,
+        help="Output artifact",
+        required=True
+    )
+
+    parser.add_argument(
+        "--output_type", 
+        type=str,
+        help="Output artifact type",
+        required=True
+    )
+
+    parser.add_argument(
+        "--output_description", 
+        type=str,
+        help="Description of the output artifact",
+        required=True
+    )
+
+    parser.add_argument(
+        "--min_price", 
+        type=float,
+        help="Minimum price to consider",
+        required=True
+    )
+
+    parser.add_argument(
+        "--max_price", 
+        type=float,
+        help="Maximum price to consider",
+        required=True
+    )
+
+    args = parser.parse_args()
+
+    go(args)
