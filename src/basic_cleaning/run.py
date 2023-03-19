8275e10ac41c1aa790ed7d7c15ac3b7f41d6b797
nanshanli <nanshan.li@toyota.com>
2023-03-19 13:28:37 -0400

    Add filter for location

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -31,6 +31,10 @@ def go(args):
     idx = df['price'].between(min_price, max_price)
     df = df[idx].copy()
 
+    # Drop rows in the dataset that are not in the area of interest
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
+
     # Saving to CSV
     df.to_csv(args.output_artifact, index=False)
     logger.info('Dataset saved.')
